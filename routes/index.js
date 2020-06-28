50fe48b3aa78fc865aa5e74c137b25cd0f4dc7a0
webdevno24 <webdevno24@gmail.com>
2020-06-29 00:29:18 +0700

    A: package sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-
-var db = mysql.createConnection({
-  host: 'localhost',
-  user: 'croot',
-  password: '1234',
-  database: 'peradiweb'
-});
-
-db.connect();
+var db = require('../database/sequelize/connection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/members/:temp', (req, res, next) => {
-  let temp = null;
-  db.query('select * from users', (err, rows, fields) => {
-    res.send({params: req.params, rows, fields});
-  });
+router.get('/members/:temp', async (req, res, next) => {
+  try {
+    await db.authenticate();
+    res.send('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 });
 
 module.exports = router;
