a846be517d49a2ec01c2f132b95a3c4c150e810f
webdevno24 <webdevno24@gmail.com>
2020-06-29 02:23:39 +0700

    A: test route get and POST with file upload

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const bodyParser = require('body-parser')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var { users } = require('../models/index');
+
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  try {
+    let data = await users.findAll();
+    res.send(data);
+  } catch (error) {
+    res.send("error"+ JSON.stringify(error));
+  }
+});
+
+router.post('/', upload.single('file'), async (req, res, next) => {
+  res.send(req.body);
 });
 
 module.exports = router;
