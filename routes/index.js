686abb4f7c6d84b4af4970d7ebf4d3ca732b929c
webdevno24 <webdevno24@gmail.com>
2020-06-29 01:36:15 +0700

    A: sequelize init

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../database/sequelize/connection');
+var db = require('../models/index').sequelize;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/members/:temp', async (req, res, next) => {
-  try {
-    await db.authenticate();
-    res.send('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  res.render('index', { title: 'Social API' });
 });
 
 module.exports = router;
